7cba09b99fe59f774d8be5e15f55e1deea5fa60e
dylan <dylan.hildenbrand@gmail.com>
2024-01-24 13:16:57 -0600

    Initial commit

---
+++ src/lib/config.js
@@ -0,0 +1,29 @@
+/**
+ * All of these values are used throughout the site – for example, 
+ * in the <meta> tags, in the footer, and in the RSS feed.
+ * 
+ * PLEASE BE SURE TO UPDATE THEM ALL! Thank you!
+ **/ 
+
+export const siteTitle = 'Personal Site Demo'
+export const siteDescription = 'A template for setting up your own personal static website'
+export const siteURL = 'http://localhost:5173'
+export const siteLink = 'http://localhost:5173'
+export const siteAuthor = 'Dylan Hildenbrand'
+
+// Controls how many posts are shown per page on the main blog index pages
+export const postsPerPage = 10
+
+// Edit this to alter the main nav menu. (Also used by the footer and mobile nav.)
+export const navItems = [
+	{
+		title: 'Blog',
+		route: '/blog'
+	}, {
+		title: 'About',
+		route: '/about'
+	}, {
+		title: 'Contact',
+		route: '/contact' 
+	},
+]
